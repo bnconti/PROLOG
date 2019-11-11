7d833e92dff94583f7b0beb163d8070eaf82c1b7
Bruno Nicolás Conti <bruno.n.conti@gmail.com>
2019-11-10 22:20:39 -0300

    Modificados algunos ejercicios

--- Racket/p02pt2.rkt
+++ Racket/p02pt2.rkt
@@ -83,17 +83,27 @@
 
 ; 7. Escribir una función que acepte una lista y devuelva su cant. de elementos.
 
-(define (cantEle lista [cont 0])
+(define (tail-tamaño lista [cont 0])
   (if (empty? lista)
       cont
-      (cantEle (rest lista) (+ cont 1))))
+      (tail-tamaño (rest lista) (+ cont 1))))
+
+(define (tamaño lst)
+  (if (empty? (rest lst))
+      1
+      (+ 1 (tamaño (rest lst)))))
 
 ; 8. Escribir una función que acepte una lista numérica y devuelva la sumatoria de la misma.
 
-(define (sumatoria lista [cont 0])
+(define (tail-sumatoria lista [cont 0])
   (if (empty? lista)
       cont
-      (sumatoria (rest lista) (+ cont (first lista)))))
+      (tail-sumatoria (rest lista) (+ cont (first lista)))))
+
+(define (sumatoria lst)
+  (if (empty? (rest lst))
+      (first lst)
+      (+ (first lst) (sumatoria (rest lst)))))
 
 ; 9. Escribir una función que acepte una lista de números enteros y devuelva
 ; la cantidad de números naturales que contiene la lista.
@@ -101,17 +111,22 @@
 (define (entero? n)
   (and (integer? n) (positive? n)))
 
-(define (cantNaturales lista)
-  (cantEle (filter entero? lista)))
+(define (cant-naturales lista)
+  (tamaño (filter entero? lista)))
 
 ; 10. Escribir una función "estaentre", que acepta dos números enteros "m" y "n", y
 ; devuelve la lista de los enteros mayores o iguales que "m" y menores o iguales que "n".
 
-(define (entre m n [lista empty])
+(define (tail-entre m n [lista empty])
   (if (equal? m (+ n 1))
       lista
-      (entre (+ m 1) n (append lista (list m)))))
-      
+      (tail-entre (+ m 1) n (append lista (list m)))))
+
+(define (entre m n)
+  (if (= m n)
+      (list m)
+      (append (list m) (entre (+ 1 m) n))))
+
 
 ; 11. Definir una función que acepta una lista y devuelve el último elemento de la misma.
 
@@ -123,7 +138,17 @@
 ; 12. Definir una función que acepte dos listas y devuelva
 ; una lista que sea la concatenación de las mismas.
 
-(define (concat lista1 lista2)
+(define (concat lst1 lst2)
+  (if (empty? lst2)
+      lst1
+      (concat (append lst1 (list (first lst2))) (rest lst2))))
+
+(define (concat2 lst1 lst2)
+  (if (empty? lst1)
+      lst2
+      (append (list (first lst1)) (concat2 (rest lst1) lst2))))
+
+(define (concat-trucho lista1 lista2)
   (append lista1 lista2))
 
 
@@ -131,16 +156,27 @@
 ; y devuelve otra lista con los elementos de la primera, pero con el átomo
 ; "a" sustituido por el "b", en su primer ocurrencia.
 
-(define (reemplazar lista a b)
-  (list-set lista (index-of lista a) b))
-  
+(define (reemplazar-uno lst a b)
+  (if (= (first lst) a)
+      (append (list b) (rest lst))
+      (append (list (first lst)) (reemplazar-uno (rest lst) a b))))
+
+(define (reemplazar-trucho lista a b)
+  (list-set lista (index-of lista a) b))  
 
 ; 14. Escribir una función que acepta dos átomos A y B y una lista C, y
 ; devuelve una lista D, tal que D es igual a C, pero con el átomo A sustituido
 ; por el B, en todas sus ocurrencias.
 
-(define (reemplazarTodos lista a b)
+(define (reemplazar-todos lst a b)
+  (if (empty? lst)
+      empty
+      (if (= (first lst) a)
+          (append (list b) (reemplazar-todos (rest lst) a b))
+          (append (list (first lst)) (reemplazar-todos (rest lst) a b)))))
+
+(define (reemplazar-todos-trucho lista a b)
   (if (pertenece? lista a)
-      (reemplazarTodos (reemplazar lista a b) a b)
+      (reemplazar-todos-trucho (reemplazar-trucho lista a b) a b)
       lista))
 
