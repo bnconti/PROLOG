2537ca171e21689df5f01f967614797f9d5a19f9
Bruno Nicolás Conti <bruno.n.conti@gmail.com>
2019-11-11 13:41:09 -0300

    Alternativas con recursión clásica para algunos ejercicios

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # PROLOG
 
 [SWI Prolog](https://www.swi-prolog.org/)
+[Racket](https://racket-lang.org/)
 
-## Notas
+## Notas Prolog
 * Para consultar un programa, `swipl nombre` o `swipl` y desp. `['nombre']`.
 * `trace` para seguir la ejecución del programa paso a paso. `notrace` para desactivar.
 * El `!` se usa para que no siga buscando soluciones.
-* Ctrl+D para hacer halt y salir del programa.
+* `Ctrl+D` para hacer halt y salir del programa.
+
+## Notas Racket
+* Usar Dr. Racket como entorno de desarrollo.
+* `Ctrl+S` para guardar y `Ctrl+R` para correr el archivo y poder llamar a las funciones.
+* `define-trace` permite seguir la ejecución de las funciones.
--- Racket/parcial.rkt
+++ Racket/parcial.rkt
@@ -9,10 +9,9 @@
 
 (define (borra x xs)
   (cond
-    ((null? xs) empty)
+    ((null? xs) xs)
     ((eq? (first xs) x) (rest xs))
-    (else
-     (cons (first xs) (borra x (rest xs))))))
+    (else (cons (first xs) (borra x (rest xs))))))
 
 (define (borra-todo x xs)
   (cond
@@ -25,16 +24,32 @@
 ; mitades en que se divide xs tales que sus longitudes difieren como máximo en uno.
 ; Por ejemplo, (mitades ‘(2 3 5 7 9)) == ‘(‘(2 3) ‘(5 7 9))
 
-(define (listaDesdeHasta xs i fin)
+(define (mitades xs)
+  (list
+   (cortar xs 0 (quotient (length xs) 2))
+   (avanzar xs (quotient (length xs) 2) (length xs))))
+
+(define (cortar xs i f)
+  (if (= i f)
+      empty
+      (cons (first xs) (cortar (rest xs) (+ i 1) f))))
+
+(define (avanzar xs i f)
+  (if (= (length xs) (- f i))
+      (cortar xs i f)
+      (avanzar (rest xs) i f)))
+
+; Esta versión hace los cortes en una sola función
+(define (cortar2 xs i fin)
   (if (eq? i fin)
       (list (first xs))
       (if (eq? i 1)
-          (cons (first xs) (listaDesdeHasta (rest xs) i (- fin 1)))
-          (listaDesdeHasta (rest xs) (- i 1) (- fin 1)))))
+          (cons (first xs) (cortar2 (rest xs) i (- fin 1)))
+          (cortar2 (rest xs) (- i 1) (- fin 1)))))
 
 (define (mitad xs)
-  (list (listaDesdeHasta xs 1 (quotient (length xs) 2))
-        (listaDesdeHasta xs (+ 1 (quotient (length xs) 2)) (length xs))))
+  (list (cortar2 xs 1 (quotient (length xs) 2))
+        (cortar2 xs (+ 1 (quotient (length xs) 2)) (length xs))))
 
 ; 3. Dos números son equivalentes si la media de sus cifras son iguales. Por ejemplo,
 ; 3205 y 41 son equvalentes ya que (3+2+0+5)/4 = (4+1)/2. Definir la función tal que
@@ -42,28 +57,48 @@
 ; (equivalentes 3205 41) -> True
 ; (equivalentes 3205 25) -> False
 
-(define (media n [sum 0] [acc 0])
-  (if (= n 0)
-      (/ sum acc)
-      (media (quotient n 10) (+ sum (remainder n 10)) (+ acc 1))))
-
 (define (equivalentes? a b)
   (= (media a) (media b)))
+
+(define (media n)
+  (/ (sumatoria n) (cifras n)))
+
+(define (sumatoria n)
+  (if (< n 10)
+      n
+      (+ (remainder n 10) (sumatoria (quotient n 10)))))
+
+(define (cifras n)
+  (if (< n 10)
+      1
+      (+ 1 (cifras (quotient n 10)))))
+
+; Con esta función puedo remplazar a "sumatoria", "cifras" y "media".
+(define (tail-media n [sum 0] [acc 0])
+  (if (= n 0)
+      (/ sum acc)
+      (tail-media (quotient n 10) (+ sum (remainder n 10)) (+ acc 1))))
 
 ; 4. El primitivo de un número se define como sigue: Dado un número natural N, multiplicamos
 ; todos sus dígitos, repetimos este procedimiento hasta que quede un solo dígito al cual
 ; llamamos primitivo de N. Por ejemplo para 327: 3x2x7 = 42 y 4x2 = 8. Por lo tanto, el
 ; primitivo de 327 es 8. Definir la función (primitivo n).
 
-(define (digitos n [prod 1])
+(define (tail-digitos n [prod 1])
   (if (= n 0)
       prod
-      (digitos (quotient n 10) (* prod (remainder n 10)))))
+      (tail-digitos (quotient n 10) (* prod (remainder n 10)))))
+
+(define (digitos n)
+  (if (= n 0)
+      1
+      (* (digitos (quotient n 10)) (remainder n 10))))
 
 (define (primitivo n)
   (if (< (digitos n) 10)
       (digitos n)
       (primitivo (digitos n))))
+
 
 ; Mayor elemento de la lista
 
