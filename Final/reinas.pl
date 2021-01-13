becc830ec73df31070a77cc31bf2bca0350d4da1
Bruno <bruno.n.conti@gmail.com>
2021-01-13 14:22:25 -0300

    Librerias faltantes, .ps para generar imagen del tablero

--- Final/reinas.pl
+++ Final/reinas.pl
@@ -1,4 +1,7 @@
 :- use_module(library(clpz)).
+:- use_module(library(lists)).
+:- use_module(library(format)).
+:- use_module(library(dcgs)).
 
 n_reinas(N, Rs) :- 
 	length(Rs, N),
@@ -19,12 +22,15 @@ reinas_seguras_aux([R|Rs], R0, D0) :-
 	
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    Ejemplos:
-   ?- n_reinas(8, Rs), labeling([ff], Qs).
+   
+   # scryer-prolog reinas.pl
+   
+   ?- n_reinas(8, Rs), labeling([ff], Rs).
       Rs = [1,5,8,6,3,7,2,4]
    ;  Rs = [1,6,8,3,7,4,2,5]
    ;  Rs = [1,7,4,6,8,2,5,3]
    ;  ...
-   ?- n_reinas(100, Qs), labeling([ff], Qs).
+   ?- n_reinas(100, Qs), labeling([ff], Rs).
       Rs = [1,3,5,57,59,4,64,7,58,71|...]
    ;  Rs = [1,3,5,57,59,4,64,7,58,71|...]
    ;  ...
